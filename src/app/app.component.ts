f8f6c645b76edf6de8b2c05b2c03fe408f0d8532
Dobroslav Radosavljevič <dradosavljevic@monetplus.cz>
2019-12-29 00:01:07 +0100

    Edited public to private props

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,18 +23,18 @@ export class AppComponent implements OnInit {
   public includeLowercase = true;
   public includeUppercase = true;
 
-  public CHAR_MIN_ASCII = 97;
-  public CHAR_MAX_ASCII = 122;
-  public SYMB_MIN_ASCII = 33;
-  public SYMB_MAX_ASCII = 47;
+  private CHAR_MIN_ASCII = 97;
+  private CHAR_MAX_ASCII = 122;
+  private SYMB_MIN_ASCII = 33;
+  private SYMB_MAX_ASCII = 47;
   public lowercaseChars = [];
   public uppercaseChars = [];
   public symbolChars = [];
-  public numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  private numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
   public generatedPassword = "";
 
-  getCharValues() {
+  private getCharValues() {
     for (
       let index = this.CHAR_MIN_ASCII;
       index <= this.CHAR_MAX_ASCII;
@@ -52,7 +52,7 @@ export class AppComponent implements OnInit {
     }
   }
 
-  copyToClipboard() {
+  public copyToClipboard() {
     if (!this.generatedPassword) {
       this._snackBar.open(
         "Generate password first!",
@@ -70,12 +70,12 @@ export class AppComponent implements OnInit {
     }
   }
 
-  getRandomCharList(arr: any[]): any[] {
+  private getRandomCharList(arr: any[]): any[] {
     const rndPosition = Math.floor(Math.random() * arr.length);
     return arr[rndPosition];
   }
 
-  generatePassword() {
+  public generatePassword() {
     if (!this.passLength) {
       this._snackBar.open(
         "Choose password length!",
