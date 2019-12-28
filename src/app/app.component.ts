03941da66d92046bd0e81e67adc4a6a2adefd3cf
Dobroslav Radosavljevič <dradosavljevic@monetplus.cz>
2019-12-28 23:58:36 +0100

    Adding project to GIT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,120 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MatSnackBar, MatSnackBarConfig } from "@angular/material/snack-bar";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'password-generator';
+export class AppComponent implements OnInit {
+  title = "password-generator";
+
+  constructor(private _snackBar: MatSnackBar) {}
+
+  private snackBarConfig: MatSnackBarConfig = {
+    horizontalPosition: "center",
+    duration: 1500,
+    verticalPosition: "top"
+  };
+
+  public passLength = 16;
+  public includeSymbols = true;
+  public includeNumbers = true;
+  public includeLowercase = true;
+  public includeUppercase = true;
+
+  public CHAR_MIN_ASCII = 97;
+  public CHAR_MAX_ASCII = 122;
+  public SYMB_MIN_ASCII = 33;
+  public SYMB_MAX_ASCII = 47;
+  public lowercaseChars = [];
+  public uppercaseChars = [];
+  public symbolChars = [];
+  public numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  public generatedPassword = "";
+
+  getCharValues() {
+    for (
+      let index = this.CHAR_MIN_ASCII;
+      index <= this.CHAR_MAX_ASCII;
+      index++
+    ) {
+      this.lowercaseChars.push(String.fromCharCode(index));
+      this.uppercaseChars.push(String.fromCharCode(index).toUpperCase());
+    }
+    for (
+      let index = this.SYMB_MIN_ASCII;
+      index <= this.SYMB_MAX_ASCII;
+      index++
+    ) {
+      this.symbolChars.push(String.fromCharCode(index));
+    }
+  }
+
+  copyToClipboard() {
+    if (!this.generatedPassword) {
+      this._snackBar.open(
+        "Generate password first!",
+        "Close",
+        this.snackBarConfig
+      );
+    } else {
+      const el = document.createElement("textarea");
+      el.value = this.generatedPassword;
+      document.body.appendChild(el);
+      el.select();
+      document.execCommand("copy");
+      document.body.removeChild(el);
+      this._snackBar.open("Copied to clipboard", "Close", this.snackBarConfig);
+    }
+  }
+
+  getRandomCharList(arr: any[]): any[] {
+    const rndPosition = Math.floor(Math.random() * arr.length);
+    return arr[rndPosition];
+  }
+
+  generatePassword() {
+    if (!this.passLength) {
+      this._snackBar.open(
+        "Choose password length!",
+        "Close",
+        this.snackBarConfig
+      );
+      return;
+    }
+    const tempPassArr = [];
+    const chars = [];
+    if (this.includeLowercase) {
+      chars.push(this.lowercaseChars);
+    }
+    if (this.includeUppercase) {
+      chars.push(this.uppercaseChars);
+    }
+    if (this.includeSymbols) {
+      chars.push(this.symbolChars);
+    }
+    if (this.includeNumbers) {
+      chars.push(this.numbers);
+    }
+    if (!chars.length) {
+      this._snackBar.open(
+        "Select atleast one option!",
+        "Close",
+        this.snackBarConfig
+      );
+    } else {
+      for (let index = 0; index < this.passLength; index++) {
+        const charList = this.getRandomCharList(chars);
+        const rndCharPosition = Math.floor(Math.random() * charList.length + 1);
+        tempPassArr.push(charList[rndCharPosition]);
+      }
+      this.generatedPassword = tempPassArr.join("");
+    }
+  }
+
+  ngOnInit() {
+    this.getCharValues();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatIconModule } from "@angular/material/icon";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { FormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSnackBarModule,
+    MatCheckboxModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
